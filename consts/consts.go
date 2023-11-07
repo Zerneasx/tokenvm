144eb3e85b53cc6d74970bd72782a11b38eb3af8
Zeno Donato <zeno16.donato@gmail.com>
2023-11-08 02:10:44 +0800

    feat: change

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Donato"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "ZenoCoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "ZC"
 )
 
 var ID ids.ID
